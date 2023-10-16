75a50ff8ca1f3321369b22a30eb94c9e210d0984
Alec Reynolds <alec+git@lando.dev>
2023-10-16 12:58:30 -0700

    Do new line without mangling things.

--- services/mariadb/builder.js
+++ services/mariadb/builder.js
@@ -62,8 +62,8 @@ module.exports = {
       };
 
       // Host is necessary to check the networked location.
-      options.healthcheck =`mysql --host=${options.name} --user=${options.creds.user}
-        --database=${options.creds.database} --password=${options.creds.password} --silent --execute "SHOW TABLES;"`;
+      options.healthcheck =`mysql --host=${options.name} --user=${options.creds.user} ` +
+        `--database=${options.creds.database} --password=${options.creds.password} --silent --execute "SHOW TABLES;"`;
       // Send it downstream
       super(id, options, {services: _.set({}, options.name, mariadb)});
     };
