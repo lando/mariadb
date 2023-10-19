e40cc2ff426d42d1bf688c839e92c7a1203a6623
Alec Reynolds <alec+git@lando.dev>
2023-10-19 13:30:05 -0700

    Feed the linter.

--- builders/mariadb.js
+++ builders/mariadb.js
@@ -43,9 +43,9 @@ module.exports = {
       // NOTE: we guard against cases where the UID is the same as the bitnami non-root user
       // because this messes things up on circle ci and presumably elsewhere and _should_ be unncessary
       if (_.get(options, '_app._config.uid', '1000') !== '1001') options._app.nonRoot.push(options.name);
-      
+
       if (!options.healthcheck) options.healthcheck = require('../utils/get-default-healthcheck')(options);
-      
+
       const mariadb = {
         image: `bitnami/mariadb:${options.version}`,
         command: '/launch.sh',
